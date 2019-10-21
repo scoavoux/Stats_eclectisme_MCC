15580c3690ad1452d98a5f62892a5b7526d2cb6d
Samuel Coavoux <samuel.coavoux@gmail.com>
2019-10-21 11:54:42 +0200

    ajouté corrections venant des algorithmes de openrefine

--- import/recodages.R
+++ import/recodages.R
@@ -46,6 +46,7 @@ for(dic in dics){
 
 m <- mutate(m, performer = case_when(
   title == "Changes" & performer == "faul & wad ad" ~ "faul & wad ad vs pnau",
+  title == "LOUIS ARMSTRONG MEETS OSC/ARMSTRONG" & performer == "armstrong" ~ "louis armstrong",
   title == "Jimmy" & performer == "cats on trees" ~ "cats on trees & calogero",
   title == "Jimmy" & performer == "cats on trees / calogero" ~ "cats on trees & calogero",
   title == "Kendji" & is.na(performer) ~ "kendji girac",
